b2fd5249152da3b0d67f61f40c8c8bacb10b8696
Yoshinori Kobayashi <yoshiiikoba@gmail.com>
2016-01-29 11:09:23 +0900

    Gemfile Add.manage it in bundler

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+
+gem 'sinatra', '~> 1.4', '>= 1.4.7'
+gem 'activerecord', '~> 4.2', '>= 4.2.5.1'
+gem 'restclient', '~> 0.11.3'
+
--- main.rb
+++ main.rb
@@ -1,7 +1,11 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'active_record'
+require 'rest_client'
+require 'rexml/document'
 
 get '/' do
+  @shibuya_shops_xml = REXML::Document.new(RestClient.get('https://www.kimonolabs.com/api/34330hpu?apikey=cg9djd7otr92ApB2hF22VSs8vEWo7vK3')).root
+
   erb :index
 end
